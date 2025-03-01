950aecabc154d890160553ff76f00872745bd65d
MBWankhade <mwankhade718@gmail.com>
2025-03-02 00:45:52 +0530

    fix

--- frontend/src/components/AddPostForm.jsx
+++ frontend/src/components/AddPostForm.jsx
@@ -30,7 +30,7 @@ const AddPostForm = () => {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
-          withCredentials: "include",
+          // withCredentials: "include",
         }
       );
       toast.success("Post created successfully!");
